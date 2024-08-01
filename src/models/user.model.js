5b228a527c991ca1cf805b104351f652968083f6
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-01 23:37:31 +0530

    Add user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchama = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Name is required!!"],
+        minlength: [3, "Name must be atleast 3 characters long!!"],
+        maxlength: [30, "Name must be less than 30 characters!!"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!!"],
+        lowercase: true,
+        minlength: [4, "Please enter a valid email address!!"],
+        maxlength: [30, "Please enter a valid email address!!"],
+        match: [/\S+@\S+\.\S+/, "Please enter a valid email address!!"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!!"],
+        minlength: [6, "Password must be atleast 3 characters long!!"],
+        maxlength: [50, "Password must be less than 50 characters!!"],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!!'],
+    }
+}, { timestamps: true });
+
+const User = mongoose.model(userSchama, "User");
+
+export default User;
