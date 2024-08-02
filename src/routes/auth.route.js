abd84a9ddce34a31c9925fb2af89e8bc3baac824
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-02 21:55:00 +0530

    Add validation middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { register, login } from '../controllers/auth.controller.js'
-import { signupSchema, loginSchema } from '../validators/auth.validator.js'
+import { register, login } from '../controllers/auth.controller.js';
+import validate from "../middlewares/validate.middleware.js";
+import { signupSchema, loginSchema } from '../validators/auth.validator.js';
 
 const router = express.Router();
 
 // Register route
-router.route("/register").post(register);
+router.route("/register").post(validate(signupSchema), register);
 
 //  Login route
-router.route("/login").post(login);
+router.route("/login").post(validate(loginSchema), login);
 
 export default router;
