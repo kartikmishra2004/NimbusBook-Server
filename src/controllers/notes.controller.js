f1925dceef09c1c4061fed6967676bf12a1e929a
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-04 16:28:20 +0530

    Read, Update and Delete Notes

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,24 +1,66 @@
 import Note from '../models/note.model.js';
 
-// Notes logic
-export const notes = async (req, res) => {
+// Notes logic - to create new notes
+export const createnotes = async (req, res) => {
     try {
         const { title, content } = req.body;
         const author = req.user._id;
         const notesData = await Note.create({ title, content, author });
-        res.status(200).json({message: "Notes Created successfully!!"});
+        res.status(200).json({ message: "Notes Created successfully!!" });
     } catch (error) {
         res.status(400).json({ message: "Failed to create notes!!" });
     }
 }
 
-// Auth notes logic - to get notes of logged in user
-export const authNotes = async (req, res) => {
+// Notes logic - to get notes of logged in user
+export const getnotes = async (req, res) => {
     try {
         const author = req.user._id;
-        const findNotes = await Note.findOne({ author: author });
+        const findNotes = await Note.find({ author: author });
         res.status(200).json(findNotes);
     } catch (error) {
         res.status(400).json({ message: "Failed to get notes!!" });
+    }
+}
+
+// Notes logic - to update notes of logged in user
+export const updatenotes = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        // Create new note object
+        const newNote = {}
+        if (title) { newNote.title = title }
+        if (content) { newNote.content = content }
+
+        // find the note to be updated and update it 
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            res.status(404).json({ message: "Failed to find notes!!" });
+        }
+        if (note.author.toString() != req.user._id) {
+            res.status(401).json({ message: "Unauthorized!!" });
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.status(200).json({ note });
+    } catch (error) {
+        res.status(400).json({ message: "Failed to update notes!!" });
+    }
+}
+
+// Notes logic - to delete notes of logged in user
+export const deletenotes = async (req, res) => {
+    try {
+        // Find the note to be deleted and delete it 
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            res.status(404).json({ message: "Failed to find notes!!" });
+        }
+        if (note.author.toString() != req.user._id) {
+            res.status(401).json({ message: "Unauthorized!!" });
+        }
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Success", note });
+    } catch (error) {
+        res.status(400).json({ message: "Failed to delete notes!!" });
     }
 }
--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import { notes, authNotes } from '../controllers/notes.controller.js';
+import { createnotes, getnotes, updatenotes, deletenotes } from '../controllers/notes.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
-// Notes endpoint
-router.route("/").post(authMiddleware, notes);
+// Notes endpoint - to create new notes for logged in user
+router.route("/create").post(authMiddleware, createnotes);
 
-// Notes endpoint - to get notes of logged in user
-router.route("/user").get(authMiddleware, authNotes);
+// Notes endpoint - to read notes of logged in user
+router.route("/get").get(authMiddleware, getnotes);
+
+// Notes endpoint - to update the notes of loggedin user
+router.route("/update/:id").put(authMiddleware, updatenotes);
+
+// Notes endpoint - to delete the notes of loggedin user
+router.route("/delete/:id").delete(authMiddleware, deletenotes);
 
 export default router;
